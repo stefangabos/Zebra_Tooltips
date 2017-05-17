7d9b5a7282bca1efb6bdb58765947e407f4d58a1
Stefan Gabos <contact@stefangabos.ro>
2017-05-17 15:32:00 +0300

    Updated example file

--- examples/examples.js
+++ examples/examples.js
@@ -3,21 +3,21 @@ $(document).ready(function() {
     new $.Zebra_Tooltips($('.zebra_tips1'));
 
     new $.Zebra_Tooltips($('.zebra_tips2'), {
-        'background_color': '#C40000',
-        'color':            '#FFF'
+        backgroundColor:    '#C40000',
+        color:              '#FFF'
     });
 
     var zt = new $.Zebra_Tooltips($('.zebra_tips3'));
     zt.show($('.zebra_tips3'), true);
 
     new $.Zebra_Tooltips($('.zebra_tips4'), {
-        'position':     'left',
-        'max_width':    300
+        position:   'left',
+        max_width:  300
     });
 
     new $.Zebra_Tooltips($('.zebra_tips5'), {
-        'position':     'right',
-        'max_width':    300
+        position:   'right',
+        max_width:  500
     });
 
 });
